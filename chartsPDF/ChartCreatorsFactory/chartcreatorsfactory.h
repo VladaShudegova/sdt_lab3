a1560ff4efd33ff48dfd8956c5185f522d12807b
Vlada Shudegova <vlada.shudegova@gmail.com>
2025-06-14 12:24:16 +0700

    Delete comments

--- chartsPDF/ChartCreatorsFactory/chartcreatorsfactory.h
+++ chartsPDF/ChartCreatorsFactory/chartcreatorsfactory.h
@@ -11,15 +11,6 @@
 using std::shared_ptr;
 using std::make_shared;
 
-// class ChartCreatorsFactory
-// {
-// public:
-//     ChartCreatorsFactory();
-//     shared_ptr<IChartCreator> getCreator(const ChartType type);
-// };
-
-
-
 class ChartCreatorsFactory {
 private:
     using CreatorFunc = std::function<std::shared_ptr<IChartCreator>()>;
